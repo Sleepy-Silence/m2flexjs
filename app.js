155cbfeabefb16b00be1b3c7b13a5ce5ecb9ad5a
ahmad <37993@ma-web.nl>
2023-11-24 12:45:31 +0100

    r

---
+++ app.js
@@ -0,0 +1,18 @@
+let canvas = document.getElementById("canvas");
+let g = canvas.getContext("2d");4
+
+
+
+
+
+
+
+
+
+
+
+
+const boardPositionSize= 50;
+const pawnPositions = [];
+const boardPositions=[];
+const playerAmountButtons = [];
